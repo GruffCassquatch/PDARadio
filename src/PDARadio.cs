969f77b271b59015824fd9dbbbf50f9a6425987a
GruffCassquatch <cassanda.agius@gmail.com>
2023-01-07 14:06:42 +1100

    Add project files.

---
+++ src/PDARadio.cs
@@ -0,0 +1,44 @@
+﻿
+using System.Reflection;
+using HarmonyLib;
+using UnityEngine;
+using Story;
+using BepInEx;
+using BepInEx.Logging;
+
+namespace PDARadio
+{
+    [BepInPlugin(myGUID, pluginName, versionString)]
+
+    public class PDARadio : BaseUnityPlugin
+    {
+            private const string myGUID = "com.GruffCassquatch.PDARadio";
+            private const string pluginName = "PDA Radio";
+            private const string versionString = "2.0.0.0";
+
+            private static readonly Harmony harmony = new Harmony(myGUID);
+
+            public static ManualLogSource logger;
+
+            private void Awake()
+            {
+                harmony.PatchAll();
+                Logger.LogInfo(pluginName + " " + versionString + " " + "loaded.");
+                logger = Logger;
+            }
+        }
+
+    [HarmonyPatch(typeof(StoryGoalManager))]
+    [HarmonyPatch("InvokePendingMessageEvent")]
+    internal class StoryGoalManager_InvokePendingMessageEvent_Patch
+    {
+        [HarmonyPostfix]
+        private static void Postfix()
+        {
+            if (GameObject.Find("EscapePod").transform.Find("ModulesRoot/RadioRoot/Radio(Clone)").gameObject.GetComponent<LiveMixin>().IsFullHealth())
+            {
+                StoryGoalManager.main.Invoke("ExecutePendingRadioMessage", 15f);
+            }
+        }
+    }
+}
